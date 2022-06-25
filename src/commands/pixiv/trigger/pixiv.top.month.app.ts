624cab883803bb0b6b9063d072f8f12e2d33a9f5
Runtus <893119806@qq.com>
2022-06-25 19:58:23 +0800

    feat: add card title & add week month pics

---
+++ src/commands/pixiv/trigger/pixiv.top.month.app.ts
@@ -0,0 +1,14 @@
+// 请参考echo文件夹内的用法
+import { AppCommand, AppFunc, BaseSession } from 'kbotify'
+import { PixivMenu } from '../components/pixiv.top.core'
+import { Mode } from '../type'
+
+class PixivTop extends AppCommand {
+    code = 'month';
+    trigger = 'month';
+    help = '.pixiv month 查看上个月top图片'
+    intro = 'pixiv top榜';
+    func: AppFunc<BaseSession> = PixivMenu(Mode.MONTH)
+}
+
+export const pixivMonth = new PixivTop();
---
+++ src/commands/pixiv/trigger/pixiv.top.week.app.ts
@@ -0,0 +1,14 @@
+// 请参考echo文件夹内的用法
+import { AppCommand, AppFunc, BaseSession } from 'kbotify'
+import { PixivMenu } from '../components/pixiv.top.core'
+import { Mode } from '../type'
+
+class PixivTop extends AppCommand {
+    code = 'week';
+    trigger = 'week';
+    help = '.pixiv week 查看上周top图片'
+    intro = 'pixiv top榜';
+    func: AppFunc<BaseSession> = PixivMenu(Mode.WEEK)
+}
+
+export const pixivWeek = new PixivTop();
